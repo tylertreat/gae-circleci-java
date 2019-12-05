9f4c9f58fa8f478f7b0b7549112cd0d96cb62319
Tyler Treat <ttreat31@gmail.com>
2019-12-05 14:31:53 -0700

    Simplify app

--- src/main/java/com/realkinetic/gaeplayground/GaePlaygroundApplication.java
+++ src/main/java/com/realkinetic/gaeplayground/GaePlaygroundApplication.java
@@ -1,31 +1,22 @@
 package com.realkinetic.gaeplayground;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 
 @SpringBootApplication
 @RestController
 public class GaePlaygroundApplication {
 
-    @Autowired
-    private RandService randomService;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(GaePlaygroundApplication.class, args);
     }
 
     @GetMapping("/")
     public String hello() {
-        randomService.callUrl();
-        return "Hello, " + randomService.randomInt();
+        return "Hello, Spring!";
     }
 
-
 }
-
